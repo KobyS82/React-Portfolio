0af5168a76dd6ccd965d8d98a2d15eb07964fb6b
KobyS82 <kobysillito@gmail.com>
2023-03-25 21:54:18 -0500

    init

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+// import React, { useState } from 'react';
+import React from 'react';
+import './style.css';
+
+function Home() {
+    return (
+        <h1>Hello World!</h1>
+    );
+}
+
+export default Home;
